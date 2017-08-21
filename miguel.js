92d5579f5c73ab29ec5b7f19cc72cbc26049c17a
Miguel Garcia-Diaz <miguel.garcia-diaz@stonybrook.edu>
2017-08-21 16:36:39 -0400

    add sum capacity

--- miguel.js
+++ miguel.js
@@ -1,1 +1,19 @@
-console.log("Hello from Miguel");
+console.log("Hello from Miguel at " + Date());
+
+
+
+//z = x+y 
+/*
+parseFloat('x.value');
+
+parseFloat('y.value');
+
+x.size=5; (changes the size of the box)
+
+
+*/
+
+x.size=y.size=10;
+x.onkeyup=y.onkeyup= function(ev){
+    z.textContent=parseFloat('x.value')+ parseFloat('y.value');
+    };
