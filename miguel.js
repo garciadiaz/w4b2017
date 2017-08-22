6d946c1ed248e7df15e344db04d6d814890a0fc6
Miguel Garcia-Diaz <miguel.garcia-diaz@stonybrook.edu>
2017-08-22 12:44:40 -0400

    add cbio method

--- miguel.js
+++ miguel.js
@@ -19,3 +19,32 @@ x.onkeyup=y.onkeyup= function(ev){
     };
 
 x.onkeypress = function(){firstName.textContent ="no"};
+
+listCancers=function(id){
+    if(!id){console.warn('id not defined')}
+    else{
+        cbio.get(
+            null,
+            function(x){
+                var div = document.getElementById(id)
+                var h = ''
+                x.forEach(function(xi, i){
+                   if(i==0){
+                     h +='<h2>' + xi + '</h2>'
+                   }else {
+                     h +='<li>' + xi + '</li>'
+                   }    
+                   })
+                   
+                div.innerHTML=h
+
+                   }
+                )
+    }
+}                
+
+
+       
+   
+
+listCancers('cancerTypesDiv')
